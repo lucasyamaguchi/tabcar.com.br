c279a89b220efdf203b963a4f9f50b736cb5f0d3
Lucas Yamaguchi <80717902+lucasyamaguchi@users.noreply.github.com>
2024-12-05 22:18:54 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <><h1>Essa mensagem vai ter surpreender, ou não!</h1></>
+    return <><h1>Essa mensagem vai ter surpreender, ou não!!!</h1></>
 }
 
 export default Home;
